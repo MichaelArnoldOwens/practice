a280fc88805190ad50191621a4eaffd1c7165fc8
Michael Arnold Owens <anpganpg@gmail.com>
2024-04-28 20:01:48 -0700

    print all the chains

--- backtracking/longest_chain.py
+++ backtracking/longest_chain.py
@@ -15,8 +15,10 @@ Note: This problem can be solved with Dynamic Programming or Backtracking. Here 
 '''
 
 def getLengthOfLongestChain(arr):
+    chain_set = set()
     def helper(start, curr_chain):
         nonlocal max_chain
+        chain_set.add(tuple(curr_chain))
         max_chain = max(max_chain, len(curr_chain))
         for i in range(start, len(arr)):
             if not curr_chain or curr_chain[-1] < arr[i]:
@@ -27,11 +29,12 @@ def getLengthOfLongestChain(arr):
     
     max_chain = 0
     helper(0, [])
+    print(list(chain_set))
     return max_chain
 
 print(getLengthOfLongestChain([1,2,3,4])) # -> 4 // [1,2,3,4]
-print(getLengthOfLongestChain([4, 3, 2, 1])) # -> 1  // [1], [2], [3], or [4]
-print(getLengthOfLongestChain([1, 3, 2, 4])) # -> 3  // [1, 3, 4] or [1, 2, 4]
-print(getLengthOfLongestChain([1, 3, 2, 4, 5, 8, 6, 7])) # -> 6  // [1, 2, 4, 5, 6, 7]
-print(getLengthOfLongestChain([10, 2, 7, 3, 6, 1, 4, 5])) # -> 4  // [2, 3, 4, 5]
+# print(getLengthOfLongestChain([4, 3, 2, 1])) # -> 1  // [1], [2], [3], or [4]
+# print(getLengthOfLongestChain([1, 3, 2, 4])) # -> 3  // [1, 3, 4] or [1, 2, 4]
+# print(getLengthOfLongestChain([1, 3, 2, 4, 5, 8, 6, 7])) # -> 6  // [1, 2, 4, 5, 6, 7]
+# print(getLengthOfLongestChain([10, 2, 7, 3, 6, 1, 4, 5])) # -> 4  // [2, 3, 4, 5]
 
