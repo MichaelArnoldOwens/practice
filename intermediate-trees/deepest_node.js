b89c460d2c45f36f411b7f94b35fe6e58336b263
Michael Arnold Owens <anpganpg@gmail.com>
2024-06-05 15:38:02 -0700

    deepest_node.js

---
+++ intermediate-trees/deepest_node.js
@@ -0,0 +1,21 @@
+function deepestNode(root) {
+
+  function helper(root) {
+    if (!root) {
+      return [null, 0];
+    }
+    if (!root.left && !root.right) {
+      return [root.value, 1];
+    }
+    const [lVal, lDepth] = helper(root.left);
+    const [rVal, rDepth] = helper(root.right);
+    if (lDepth > rDepth) {
+      return [lVal, lDepth + 1];
+    } else {
+      return [rVal, rDepth + 1];
+    }
+  }
+
+  const [val, _] = helper(root);
+  return val;
+}
